f680c15bd3d075073133a5d3b86a8de5a3deedd0
Harsh Ajay <121073750+Herohasu@users.noreply.github.com>
2024-10-01 15:11:41 +0530

    Update ApiEndpoint.js

--- client/src/services/ApiEndpoint.js
+++ client/src/services/ApiEndpoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://project-pulse-25ga.onrender.com',
   headers: {
   'Content-Type': 'application/json',
   },
